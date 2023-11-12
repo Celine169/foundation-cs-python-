ec840dee80e3af917e9ce7b3242463b40fb567a7
Celine <celinenazha169@gmail.com>
2023-11-12 17:18:03 +0200

    second commit

--- Celine Nazha - Midterm Solutions/main.py
+++ Celine Nazha - Midterm Solutions/main.py
@@ -1,3 +1,25 @@
+def activateChoice(choice):
+  if choice == 1:
+    openTab()
+  elif choice == 2:
+    closeTab()
+  elif choice == 3:
+    switchTab()
+  elif choice == 4:
+    displayAllTabs()
+  elif choice == 5:
+    openNestedTabs()
+  elif choice == 6:
+    clearAllTabs()
+  elif choice == 7:
+    saveTabs()
+    
+  elif choice == 8:
+    importTabs()
+  elif choice == 9:
+    print("Exiting program,Goodbye! ")
+    exit()
+
 def main():
   print("\n")
   print('1. Open Tab')
@@ -16,4 +38,3 @@ def main():
 
 print('Hello, welcome to the program!')
 main()
-
