55c68c295f8ec2257fbcbcd511d147f8c0656565
Celine <celinenazha169@gmail.com>
2023-11-12 17:48:05 +0200

    applying openTab function

--- Celine Nazha - Midterm Solutions/main.py
+++ Celine Nazha - Midterm Solutions/main.py
@@ -9,7 +9,11 @@ def validateChoice(choice):
 
 
 def openTab():
-  print('the tab opened')
+  title = input('Enter the tab title : ')
+  url = input('Enter the url tab  : ')
+  Tabs.append({'title': title, 'url': url, 'nestedTabs': []})
+  print(f"Tab '{title}' with URL '{url}' created successfully.")
+  main()
 
 def closeTab():
  print('the tab closed')
@@ -35,7 +39,7 @@ def importTabs():
 def exit():
   print('the program exited')
 
-  
+
 def activateChoice(choice):
   if choice == 1:
     openTab()
